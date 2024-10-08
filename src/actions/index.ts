e226a15d4718a12a2156871b5fa5ffc8b4b85d20
Gabriel Melo <bielrj170@gmail.com>
2024-10-07 23:15:27 -0300

    feat: add a ts dbActions

---
+++ src/actions/index.ts
@@ -0,0 +1,34 @@
+"use server";
+
+import { Products } from "@/context/mycontext";
+import db from "../../prisma/db";
+import { revalidatePath } from "next/cache";
+
+export async function getAll() {
+	try {
+		const response = await db.product.findMany();
+		return response;
+	} catch (error) {
+		console.error("Error fetching products:", error);
+		return undefined;
+	}
+}
+
+export async function increment(product: Products) {
+	try {
+		await db.product.update({
+			where: {
+				id: product.id,
+			},
+			data: {
+				likes: {
+					increment: 1,
+				},
+			},
+		});
+
+		revalidatePath("/");
+	} catch (err) {
+		console.error("Error incrementing likes:", err);
+	}
+}

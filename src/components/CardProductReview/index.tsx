42a8dbf3a6d137c2b895b88678ab82f5b81db529
Gabriel Melo <bielrj170@gmail.com>
2024-10-08 16:37:46 -0300

    refactor: refactor image url

--- src/components/CardProductReview/index.tsx
+++ src/components/CardProductReview/index.tsx
@@ -1,6 +1,5 @@
+import useMyContext from "@/hooks/useMyContext";
 import Image from "next/image";
-import logo from "../../app/logo.png";
-
 interface CardProductReview {
 	productName: string;
 	productDescription: string;
@@ -11,9 +10,10 @@ const CardProductReview: React.FC<CardProductReview> = ({
 	productDescription,
 	productLikes,
 }) => {
+	const { productsData } = useMyContext()!;
 	return (
 		<div className="mb-4 p-4 flex flex-row items-start gap-2 border rounded shadow-md">
-			<Image alt="Logo" src={logo} width={50} height={50} />
+			<Image alt="Logo" src={productsData![0].url} width={50} height={50} />
 			<div className="flex-1">
 				<p className="font-bold">{productName}</p>
 				<p className="text-gray-600">{productDescription}</p>

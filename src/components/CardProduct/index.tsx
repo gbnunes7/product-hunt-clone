6e2d9efb101ca68dcc385b3c9998e6bd793a18d8
Gabriel Melo <bielrj170@gmail.com>
2024-10-07 23:18:58 -0300

    refactor: add props on upvote

--- src/components/CardProduct/index.tsx
+++ src/components/CardProduct/index.tsx
@@ -1,19 +1,25 @@
 import Image from "next/image";
 import logo from "../../app/logo.png";
 import UpVote from "../Upvote";
+import { Product } from "@prisma/client";
 
 interface CardProductProps {
 	productName: string;
 	productDescription: string;
-	productLikes: string;
-	productTags: string[];
+	productLikes: number;
+	productTags: string;
+	product: Product;
+	index: number
 }
+
 
 const CardProduct: React.FC<CardProductProps> = ({
 	productName,
 	productDescription,
 	productTags,
 	productLikes,
+	product,
+	index,
 }) => {
 	return (
 		<div className="mb-4 p-4 flex flex-row items-start gap-2 border rounded shadow-md">
@@ -22,17 +28,12 @@ const CardProduct: React.FC<CardProductProps> = ({
 				<span className="font-bold">{productName}</span>
 				<p className="text-gray-600">{productDescription}</p>
 				<div className="flex flex-wrap mt-2">
-					{productTags.map((tag, index) => (
-						<span
-							key={index}
-							className="bg-gray-200 text-gray-700 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded"
-						>
-							{tag}
-						</span>
-					))}
+					<span className="bg-gray-200 text-gray-700 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded">
+						{productTags}
+					</span>
 				</div>
 			</div>
-			<UpVote likes={productLikes} />
+			<UpVote index={index} product={product} likes={productLikes} />
 		</div>
 	);
 };

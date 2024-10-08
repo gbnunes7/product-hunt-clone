470c89cbc8e72d4146f5bc529ca3dc900889522e
Gabriel Melo <bielrj170@gmail.com>
2024-10-07 23:20:48 -0300

    refactor: refactor mocked data to data from db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,55 +5,11 @@ import CardProductReview from "@/components/CardProductReview";
 import FilterBanner from "@/components/FilterBanner";
 import ListItem from "@/components/ListItem";
 import Title from "@/components/Title";
+import { MyContext } from "@/context/mycontext";
+import { useContext } from "react";
 
 export default function Home() {
-	const products = [
-		{
-			id: 1,
-			nome: "Camiseta Estampada",
-			descricao: "Camiseta de algodão com estampa criativa.",
-			tags: ["moda", "estampa", "algodão"],
-			likes: "150",
-			review: true,
-		},
-		{
-			id: 2,
-			nome: "Tênis Running",
-			descricao: "Tênis leve e confortável para corridas.",
-			tags: ["esporte", "corrida", "conforto"],
-			likes: "230",
-			review: true,
-		},
-		{
-			id: 3,
-			nome: "Relógio Smart",
-			descricao: "Relógio inteligente com monitoramento de atividades.",
-			tags: ["tecnologia", "fitness", "wearable"],
-			likes: "320",
-			review: false,
-		},
-		{
-			id: 4,
-			nome: "Mochila Adventure",
-			descricao: "Mochila resistente ideal para trilhas e aventuras.",
-			tags: ["aventura", "mochila", "trilha"],
-			likes: "180",
-			review: false,
-		},
-		{
-			id: 5,
-			nome: "Fone Bluetooth",
-			descricao: "Fones sem fio com cancelamento de ruído.",
-			tags: ["tecnologia", "audio", "sem fio"],
-			likes: "400",
-			review: true,
-		},
-	];
-
-	const productsReview = products.filter((product) => product.review);
-
-	products.sort((a, b) => Number(b.likes) - Number(a.likes));
-	productsReview.sort((a, b) => Number(b.likes) - Number(a.likes));
+	const { productsData, likes } = useContext(MyContext)!;
 
 	return (
 		<main className="px-4 my-8 flex flex-col w-full overflow-auto">
@@ -66,19 +22,22 @@ export default function Home() {
 						</Title>
 					</div>
 					<ul>
-						{products.map((item) => (
-							<ListItem key={item.id}>
-								<CardProduct
-									productDescription={item.descricao}
-									productName={item.nome}
-									productTags={item.tags}
-									productLikes={item.likes}
-								/>
-							</ListItem>
-						))}
+						{productsData &&
+							productsData.map((item, index) => (
+								<ListItem key={index}>
+									<CardProduct
+										index={index}
+										productDescription={item.description}
+										productName={item.name}
+										productTags={item.tag}
+										productLikes={likes[index]}
+										product={item}
+									/>
+								</ListItem>
+							))}
 					</ul>
 				</div>
-				{productsReview.length > 0 && (
+				{productsData && (
 					<div className="flex flex-col md:w-[25%]">
 						<div className="flex items-center justify-center my-8 border-b-[1px] md:border-b-0 pb-[15px]">
 							<Title level={3} className="text-black font-bold text-2xl">
@@ -86,15 +45,17 @@ export default function Home() {
 							</Title>
 						</div>
 						<ul>
-							{productsReview.map((item) => (
-								<ListItem key={item.id}>
-									<CardProductReview
-										productDescription={item.descricao}
-										productName={item.nome}
-										productLikes={item.likes}
-									/>
-								</ListItem>
-							))}
+							{productsData
+								.filter((item) => item.review)
+								.map((item) => (
+									<ListItem key={item.id}>
+										<CardProductReview
+											productDescription={item.description}
+											productName={item.name}
+											productLikes={item.likes}
+										/>
+									</ListItem>
+								))}
 						</ul>
 					</div>
 				)}

d66944e582090ccaa7e8b187ad96398de2ade89c
Gabriel Melo <bielrj170@gmail.com>
2024-10-09 16:34:00 -0300

    refactor: move interface to specific folder

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,17 +1,5 @@
-interface InputProps {
-	className: string;
-	type: string;
-	name: string;
-	id: string;
-	onSubmit?: () => void;
-	placeholder: string;
-	autoComplete: string;
-	value: any;
-	onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
-	required?: boolean;
-	minLength?: number;
-	maxLength?: number;
-}
+import { InputProps } from "@/interface/inputProps";
+
 const InputSearch: React.FC<InputProps> = ({ ...rest }) => {
 	return <input {...rest} />;
 };

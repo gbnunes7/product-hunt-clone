72b32f47a9f9309ead3a153f20478c9487e5023f
Gabriel Melo <bielrj170@gmail.com>
2024-10-09 16:33:54 -0300

    refactor: move interface to specific folder

--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -1,8 +1,4 @@
-interface ListItemProps {
-	children: React.ReactNode;
-	className?: string;
-}
-
+import { ListItemProps } from "@/interface/listItemProps";
 const ListItem: React.FC<ListItemProps> = ({ children, ...rest }) => {
 	return <li {...rest}>{children}</li>;
 };
